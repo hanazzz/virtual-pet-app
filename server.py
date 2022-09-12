5b86fe34fd82bc9a1af320269585ce97dd07d0e4
Hana Zait <hana.o.zait@gmail.com>
2022-09-12 15:09:16 -0700

    Finished skeleton for server.py

--- server.py
+++ server.py
@@ -3,12 +3,25 @@
 from flask import (Flask, render_template, request, flash, session, redirect)
 
 from model import connect_to_db, db
-# import crud
+import crud
 from jinja2 import StrictUndefined
 
 # create Flask app
 app = Flask(__name__)
+# create secret_key (req for session object)
+app.secret_key = "lkdhjfasiop89ryweq23809"
+# throw error for undefined variables
+app.jinja_env.undefined = StrictUndefined
+
+
+# ROUTES AND VIEW FUNCTIONS GO HERE
+@app.route('/')
+def show_homepage():
+    """Show homepage."""
 
 
 if __name__ == "__main__":
+    # connect app to db
+    connect_to_db(app)
+    # run app
     app.run(debug=True, host='0.0.0.0')
