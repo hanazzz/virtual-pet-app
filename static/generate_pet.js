d228873395c43fe5e971fde3f1899d91caba9934
Hana Zait <hana.o.zait@gmail.com>
2022-09-13 18:07:59 -0700

    Set up Generate Pet button to receive pet data from server and display it. Set up Adopt Pet button to send pet data to server.

--- static/generate_pet.js
+++ static/generate_pet.js
@@ -1,28 +1,79 @@
 generatePetButton = document.querySelector("#generate-pet");
-petInfoTable = document.querySelector("#pet-info");
-
-generatePetButton.addEventListener("click", () => {
-    fetch("/generate-pet")
-    .then((response) => response.json())
-    .then((petData) => {
-        petInfoTable.innerHTML = "";
-        for (key in petData) {
-            console.log(key);
-            console.log(petData[key]);
-            let tableRows = `
-            <tr>
-                <td>${key}</td>
-                <td>${petData[key]}</td>
-            </tr>`;
-            petInfoTable.insertAdjacentHTML('beforeEnd', tableRows);
-        }
-
-        let currentPetData = petData;
-    })
-});
-
 adoptPetButton =  document.querySelector("#adopt-pet");
 
-// adoptPetButton.addEventListener("click", () => {
-//     console.log(currentPetData);
-// });
+petInfoTable = document.querySelector("#pet-info");
+
+let currentPetData = null;
+
+generatePetButton.addEventListener("click", () => {
+        fetch("/generate-pet")
+        .then((response) => response.json())
+        .then((petData) => {
+            console.log(petData);
+            // Update species img
+            document.querySelector("#species-img").src = petData["Img path"]
+
+            // Update pet info table
+            petInfoTable.innerHTML = "";
+            let tableRows = `
+                <tr>
+                    <td>Pet species</td>
+                    <td>${petData["Pet species"]}</td>
+                </tr>
+                <tr>
+                    <td>Favorite food</td>
+                    <td>${petData["Favorite food"]}</td>
+                </tr>
+                <tr>
+                    <td>Least favorite food</td>
+                    <td>${petData["Least favorite food"]}</td>
+                </tr>
+                <tr>
+                    <td>Favorite activity</td>
+                    <td>${petData["Favorite activity"]}</td>
+                </tr>
+                <tr>
+                    <td>Least favorite activity</td>
+                    <td>${petData["Least favorite activity"]}</td>
+                </tr>
+                <tr>
+                    <td>Favorite music genre</td>
+                    <td>${petData["Favorite music genre"]}</td>
+                </tr>
+                <tr>
+                    <td>Least favorite music genre</td>
+                    <td>${petData["Least favorite music genre"]}</td>
+                </tr>
+                <tr>
+                    <td>Favorite weather</td>
+                    <td>${petData["Favorite weather"]}</td>
+                </tr>
+                <tr>
+                    <td>Least favorite weather</td>
+                    <td>${petData["Least favorite weather"]}</td>
+                </tr>
+                <tr>
+                    <td>Personality</td>
+                    <td>${petData["Personality"]}</td>
+                </tr>
+                <tr>
+                    <td>Astrological sign</td>
+                    <td>${petData["Astrological sign"]}</td>
+                </tr>
+            `;
+            petInfoTable.insertAdjacentHTML('beforeEnd', tableRows);
+
+            currentPetData = petData;
+
+             // When user clicks adopt pet button, send pet data back to server
+            adoptPetButton.addEventListener("click", () => {
+                fetch("/adopt-pet", {
+                    method: 'POST',
+                    body: JSON.stringify(currentPetData),
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                })
+            });
+    })
+});
