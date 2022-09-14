10ecd937c3ad60d7a93f25a364cc31b2081fd6ab
Hana Zait <hana.o.zait@gmail.com>
2022-09-14 15:22:55 -0700

    Added ids to pet info table cells, moved addPetButton evt listener out of fetch request, solved bug of multiple POST requests with pet data

--- static/generate_pet.js
+++ static/generate_pet.js
@@ -3,8 +3,6 @@ adoptPetButton =  document.querySelector("#adopt-pet");
 
 petInfoTable = document.querySelector("#pet-info");
 
-let currentPetData = null;
-
 generatePetButton.addEventListener("click", () => {
         fetch("/generate-pet")
         .then((response) => response.json())
@@ -18,65 +16,79 @@ generatePetButton.addEventListener("click", () => {
             let tableRows = `
                 <tr>
                     <td>Pet species</td>
-                    <td>${petData["Pet species"]}</td>
+                    <td id="pet-species">${petData["Pet species"]}</td>
                 </tr>
                 <tr>
                     <td>Favorite food</td>
-                    <td>${petData["Favorite food"]}</td>
+                    <td id="food-fave">${petData["Favorite food"]}</td>
                 </tr>
                 <tr>
                     <td>Least favorite food</td>
-                    <td>${petData["Least favorite food"]}</td>
+                    <td id="food-least">${petData["Least favorite food"]}</td>
                 </tr>
                 <tr>
                     <td>Favorite activity</td>
-                    <td>${petData["Favorite activity"]}</td>
+                    <td id="activity-fave">${petData["Favorite activity"]}</td>
                 </tr>
                 <tr>
                     <td>Least favorite activity</td>
-                    <td>${petData["Least favorite activity"]}</td>
+                    <td id="activity-least">${petData["Least favorite activity"]}</td>
                 </tr>
                 <tr>
                     <td>Favorite music genre</td>
-                    <td>${petData["Favorite music genre"]}</td>
+                    <td id="music-fave">${petData["Favorite music genre"]}</td>
                 </tr>
                 <tr>
                     <td>Least favorite music genre</td>
-                    <td>${petData["Least favorite music genre"]}</td>
+                    <td id="music-least">${petData["Least favorite music genre"]}</td>
                 </tr>
                 <tr>
                     <td>Favorite weather</td>
-                    <td>${petData["Favorite weather"]}</td>
+                    <td id="weather-fave">${petData["Favorite weather"]}</td>
                 </tr>
                 <tr>
                     <td>Least favorite weather</td>
-                    <td>${petData["Least favorite weather"]}</td>
+                    <td id="weather-least">${petData["Least favorite weather"]}</td>
                 </tr>
                 <tr>
                     <td>Personality</td>
-                    <td>${petData["Personality"]}</td>
+                    <td id="personality">${petData["Personality"]}</td>
                 </tr>
                 <tr>
                     <td>Astrological sign</td>
-                    <td>${petData["Astrological sign"]}</td>
+                    <td id="astro-sign">${petData["Astrological sign"]}</td>
                 </tr>
             `;
             petInfoTable.insertAdjacentHTML('beforeEnd', tableRows);
+    })
+});
 
-            currentPetData = petData;
+adoptPetButton.addEventListener("click", () => {
+    let currentPetData = {
+        "Pet species" : document.getElementById("pet-species").textContent,
+        "Species img path" : document.getElementById("species-img").src,
+        "Favorite food" : document.getElementById("food-fave").textContent,
+        "Least favorite food" : document.getElementById("food-least").textContent,
+        "Favorite activity" : document.getElementById("activity-fave").textContent,
+        "Least favorite activity" : document.getElementById("activity-least").textContent,
+        "Favorite music genre" : document.getElementById("music-fave").textContent,
+        "Least favorite music genre" : document.getElementById("music-least").textContent,
+        "Favorite weather" : document.getElementById("weather-fave").textContent,
+        "Least favorite weather" : document.getElementById("weather-least").textContent,
+        "Personality" : document.getElementById("personality").textContent,
+        "Astrological sign" : document.getElementById("astro-sign").textContent,
+        "Name" : null,
+        "Zipcode" : null
+    }
 
-             // When user clicks adopt pet button, prompt for pet name and
-            //  zipcode, send pet data back to server
-            adoptPetButton.addEventListener("click", () => {
-                currentPetData["Name"] = prompt("Please name your pet:");
-                currentPetData["Zipcode"] = prompt("Where would you like your pet to live? (Enter zipcode)");
-                fetch("/adopt-pet", {
-                    method: 'POST',
-                    body: JSON.stringify(currentPetData),
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                })
-            });
+    console.log("adding evt listener")
+    currentPetData["Name"] = prompt("Please name your pet:");
+    currentPetData["Zipcode"] = prompt("Where would you like your pet to live? (Enter zipcode)");
+    fetch("/adopt-pet", {
+        method: 'POST',
+        body: JSON.stringify(currentPetData),
+        headers: {
+            'Content-Type': 'application/json',
+        },
     })
 });
