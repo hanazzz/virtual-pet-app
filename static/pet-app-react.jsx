955c16779fcac4bbeb3b816e19f02ddf022454e5
Hana Zait <hana.o.zait@gmail.com>
2022-09-16 18:22:01 -0700

    Set up log in/create acct forms to be handled through React. Need to add Fetch request to callback fctn.

--- static/pet-app-react.jsx
+++ static/pet-app-react.jsx
@@ -1,8 +1,13 @@
 function AcctForm(props) {
   // LOGIC FOR LOG IN / CREATE ACCOUNT BUTTON
+  // logInMethd tracks whether the user is logging into an existing account
+  // or creating a new account (initla value is logging in)
   const [logInMethd, setLogInMethd] = React.useState("Log in");
+  // btnMsg is the button text to switch forms
   const [btnMsg, setBtnMsg] = React.useState("New user? Create an account instead.");
 
+  // Whatever the current logInMethd is, this switches it to the opposite.
+  // btnMsg updates accordingly
   function switchLogInMethd() {
     if (logInMethd == "Log in") {
       setLogInMethd("Create account");
@@ -13,15 +18,51 @@ function AcctForm(props) {
     }
   }
 
+  // DEFINE STATES FOR FORM FIELDS
+  // Initial state value is empty string
+  const [username, setUsername] = React.useState("")
+  const [email, setEmail] = React.useState("")
+  const [password, setPassword] = React.useState("")
+  const [password2, setPassword2] = React.useState("")
+
+  // CALLBACK FUNCTION FOR FORM SUBMISSION
+  // Prevents default behavior (page refresh)
+  // and sends form data to server via Fetch re quest
+  function submitForm(evt) {
+    evt.preventDefault();
+    console.log("Form submission");
+    console.log(username, email, password, password2)
+  }
+
   // DEFINE FORMS
+  // Have state for each form field.
+  // Use onChange attribute for fields to update state with setState.
+  // Use onSubmit for entire form to handle form submission.
   const logInForm = (<div>
   <h2>Log In</h2>
-  <form action="/login" method="POST">
+  <form onSubmit={(evt) => submitForm(evt)}>
       <label htmlFor="user-username">Username:</label>
-      <input type="text" name="username" maxLength="30" required="required" aria-required="true" id="user-username" /><br />
+      <input
+      type="text"
+      name="username"
+      maxLength="30"
+      required="required"
+      aria-required="true"
+      id="user-username"
+      value={username}
+      onChange={(evt) => setUsername(evt.target.value)} />
+      <br />
 
       <label htmlFor="user-password">Password:</label>
-      <input type="password" name="password" required="required" aria-required="true" id="user-password" />
+      <input
+      type="password"
+      name="password"
+      required="required"
+      aria-required="true"
+      id="user-password" 
+      value={password}
+      onChange={(evt) => setPassword(evt.target.value)}/>
+      <br />
 
       <input type="submit" />
   </form>
@@ -30,31 +71,64 @@ function AcctForm(props) {
   const createAcctForm = (<div>
   <h2>Create Account</h2>
 
-  <form action="/create-user" method="POST">
+  <form onSubmit={(evt) => submitForm(evt)}>
       <label htmlFor="user-email-new">Email:</label>
-      <input type="email" name="email" maxLength="254" required="required" aria-required="true" id="user-email-new" /><br />
+      <input
+      type="email"
+      name="email"
+      maxLength="254"
+      required="required"
+      aria-required="true"
+      id="user-email-new"
+      value={email}
+      onChange={(evt) => setEmail(evt.target.value)} />
+      <br />
 
       <label htmlFor="user-username-new">Username:</label>
-      <input type="text" name="username" maxLength="30" required="required" aria-required="true" id="user-username-new" /><br />
+      <input
+      type="text"
+      name="username"
+      maxLength="30"
+      required="required"
+      aria-required="true"
+      id="user-username-new"
+      value={username}
+      onChange={(evt) => setUsername(evt.target.value)} />
+      <br />
 
       <label htmlFor="user-password-new">Password:</label>
-      <input type="password" name="password" maxLength="30" required="required" aria-required="true" id="user-password-new" /><br />
+      <input
+      type="password"
+      name="password"
+      maxLength="30"
+      required="required"
+      aria-required="true"
+      id="user-password-new"
+      value={password}
+      onChange={(evt) => setPassword(evt.target.value)} />
+      <br />
 
       <label htmlFor="user-password-confirm">Confirm password:</label>
-      <input type="password" name="password2" required="required" aria-required="true" id="user-password-confirm" />
+      <input
+      type="password"
+      name="password2"
+      required="required"
+      aria-required="true"
+      id="user-password-confirm"
+      value={password2}
+      onChange={(evt) => setPassword2(evt.target.value)} />
+      <br />
 
       <input type="submit" />
   </form>
   </div>);
-  let form = "null";
+  let form = null;
 
   // FORM LOGIC
   if (logInMethd == "Log in") {
     form = logInForm;
-    console.log(form);
   } else if (logInMethd == "Create account") {
     form = createAcctForm;
-    console.log(form);
   }
 
   return (
