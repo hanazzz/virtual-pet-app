439c2a61110fd4f0e6d2ba5dd06281a85664b996
Hana Zait <hana.o.zait@gmail.com>
2022-09-19 14:57:02 -0700

    Set up retrieving user's pet data and displaying current pet data. Need to complete pet generator.

--- static/pet-app-react.jsx
+++ static/pet-app-react.jsx
@@ -1,52 +1,60 @@
-function PetDisplay() {
+function PetDisplay(props) {
 
   return (
-    <table>
-      <tr>
-          <td>Pet species</td>
-          <td id="pet-species">TBD</td>
-      </tr>
-      <tr>
-          <td>Favorite food</td>
-          <td id="food-fave">TBD</td>
-      </tr>
-      <tr>
-          <td>Least favorite food</td>
-          <td id="food-least">TBD</td>
-      </tr>
-      <tr>
-          <td>Favorite activity</td>
-          <td id="activity-fave">TBD</td>
-      </tr>
-      <tr>
-          <td>Least favorite activity</td>
-          <td id="activity-least">TBD</td>
-      </tr>
-      <tr>
-          <td>Favorite music genre</td>
-          <td id="music-fave">TBD</td>
-      </tr>
-      <tr>
-          <td>Least favorite music genre</td>
-          <td id="music-least">TBD</td>
-      </tr>
-      <tr>
-          <td>Favorite weather</td>
-          <td id="weather-fave">TBD</td>
-      </tr>
-      <tr>
-          <td>Least favorite weather</td>
-          <td id="weather-least">TBD</td>
-      </tr>
-      <tr>
-          <td>Personality</td>
-          <td id="personality">TBD</td>
-      </tr>
-      <tr>
-          <td>Astrological sign</td>
-          <td id="astro-sign">TBD</td>
-      </tr>
-    </table>
+    <div>
+      <div>
+        <img src={`${props.pet.species_img_path}`} alt={`${props.pet.species_name}`} id="species-img" />
+        <p>Images created using <a href="https://www.craiyon.com/">Craiyon</a></p>
+      </div>
+      <table>
+        <tbody>
+          <tr>
+            <td>Pet species</td>
+            <td id="pet-species">{props.pet.species_name}</td>
+          </tr>
+          <tr>
+              <td>Favorite food</td>
+              <td id="food-fave">TBD</td>
+          </tr>
+          <tr>
+              <td>Least favorite food</td>
+              <td id="food-least">TBD</td>
+          </tr>
+          <tr>
+              <td>Favorite activity</td>
+              <td id="activity-fave">TBD</td>
+          </tr>
+          <tr>
+              <td>Least favorite activity</td>
+              <td id="activity-least">TBD</td>
+          </tr>
+          <tr>
+              <td>Favorite music genre</td>
+              <td id="music-fave">TBD</td>
+          </tr>
+          <tr>
+              <td>Least favorite music genre</td>
+              <td id="music-least">TBD</td>
+          </tr>
+          <tr>
+              <td>Favorite weather</td>
+              <td id="weather-fave">TBD</td>
+          </tr>
+          <tr>
+              <td>Least favorite weather</td>
+              <td id="weather-least">TBD</td>
+          </tr>
+          <tr>
+              <td>Personality</td>
+              <td id="personality">TBD</td>
+          </tr>
+          <tr>
+              <td>Astrological sign</td>
+              <td id="astro-sign">TBD</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
 
@@ -57,47 +65,51 @@ function GeneratePet() {
   return (
     <div>
       no pet!
+      <PetDisplay />
     </div>
   )
 }
 
 
-function CurrentPet() {
-  alert("Your pet is so cute!")
-  // <button id="delete-pet">DELETE PET</button>
+function CurrentPet(props) {
+  alert("Your pet is so cute!");
 
   return (
     <div>
-      meow! here is your pet!
+      <h1>meow! here is your pet!</h1>
+      <h2>{props.pet.name} the {props.pet.personality} {props.pet.species_name}</h2>
+      <h3 id="location">Location: {props.pet.city}, {props.pet.region}, {props.pet.country}</h3>
+      <PetDisplay pet={props.pet} />
+  
+      <button id="delete-pet">DELETE PET</button>
     </div>
   )
 }
 
 
 function VirtualPetApp() {
-  // const [userID, setUserID] = React.useState(0)
-  const [hasPet, setHasPet] = React.useState();
+  const [petData, setPetData] = React.useState();
 
   React.useEffect(() => {
     fetch("/user-info")
       .then((response) => response.json())
-      .then((petData) => {
-        console.log(petData);
-
-        if (petData) {
-          setHasPet(true);
-        }
+      .then((petJson) => {
+        if (petJson) {
+          setPetData(petJson);
+        } 
       })
   }, []);
   
-  if (hasPet) {
+  if (petData) {
     return (
-      <CurrentPet />
+      <CurrentPet pet={petData} />
+    )
+  } else if (petData == false) {
+    return (
+      <GeneratePet petData="TBD" />
     )
   } else {
-    return (
-      <GeneratePet />
-    )
+    return null
   }
 }
 
