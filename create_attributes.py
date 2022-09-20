85191a7c0fcedba87abcf6666f834721cf019be6
Hana Zait <hana.o.zait@gmail.com>
2022-09-20 11:34:28 -0700

    Created files of pet characteristics and function to create lists from files (instead of hardcoding lists)

---
+++ create_attributes.py
@@ -0,0 +1,44 @@
+"""Create lists of pet attributes from text files."""
+
+def make_list(file_path, list_name):
+    """Create list of pet attributes from file.
+
+    Arguments:
+    - file_path (str): Path to txt file of attributes
+    - list_name (lst): List to which to add attributes
+
+    Return:
+    - list_name (lst)
+    """
+
+    list_name = []
+
+    with open(file_path) as f:
+        for line in f:
+            line = line.rstrip()
+            list_name.append(line)
+
+    return list_name
+
+SPECIES = {
+    "sparkle-wolf" : ["Sparkle Wolf", "/static/images/sparkle-wolf.jpg"],
+    "prickly-bunny" : ["Prickly Bunny", "/static/images/prickly-bunny.jpg"],
+    "invisi-cat" : ["Invisible Cat", "/static/images/invisi-cat.jpg"]
+}
+
+SPECIES_ID = list(SPECIES.keys())
+
+FOOD = make_list("data/food.txt", "FOOD")
+
+ACTIVITY = make_list("data/activities.txt", "ACTIVITY")
+
+MUSIC_GENRE = make_list("data/music_genres.txt", "MUSIC_GENRES")
+
+WEATHER = make_list("data/weather.txt", "WEATHER")
+
+PERSONALITY = make_list("data/personality.txt", "PERSONALITY")
+
+ASTROLOGICAL_SIGN = [
+    "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio",
+    "Sagittarius", "Capricorn", "Aquarius", "Pisces"
+]
