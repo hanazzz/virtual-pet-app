5f0d383d1159a048c354094d1e476357de9bd40e
Hana Zait <hana.o.zait@gmail.com>
2022-09-21 16:44:15 -0700

    Pulled CurrentPet component into separate file.

---
+++ static/react/CurrentPet.jsx
@@ -0,0 +1,43 @@
+/* eslint-env browser */
+/* global React PropTypes WeatherDisplay PetDisplay */
+/* eslint no-else-return: "error" */
+// REMOVE THE BELOW BEFORE DEPLOYMENT
+/* eslint-disable no-console */
+/* eslint-disable no-alert */
+
+function CurrentPet(props) {
+  const { pet } = props;
+  const { setPetData } = props;
+  CurrentPet.propTypes = {
+    pet: PropTypes.object.isRequired,
+    setPetData: PropTypes.func.isRequired,
+  };
+
+  console.log('Existing pet data, rendering CurrentPet');
+
+  function deletePet() {
+    if (confirm('Are you sure you want to delete your pet? This action is irreversible.')) {
+      fetch('/delete-pet')
+        .then((response) => response.json())
+        .then((msg) => {
+          console.log('deleting pet');
+          alert(msg);
+          setPetData(null);
+        })
+        .catch((error) => alert(error.toString()));
+    } else {
+      alert('Your pet has not been deleted.');
+    }
+  }
+
+  return (
+    <div>
+      <h1>Your Pet</h1>
+      <h2>{pet.name} the {pet.personality} {pet.species_name}</h2>
+      <h3 id="location">Location: {pet.city}, {pet.region}, {pet.country}</h3>
+      <PetDisplay pet={pet} />
+      <WeatherDisplay pet={pet} />
+      <button type="button" id="delete-pet" onClick={deletePet}>DELETE PET</button>
+    </div>
+  );
+}
--- static/react/app.jsx
+++ static/react/app.jsx
@@ -176,43 +176,6 @@ function PetGenerator(props) {
   );
 }
 
-function CurrentPet(props) {
-  const { pet } = props;
-  const { setPetData } = props;
-  CurrentPet.propTypes = {
-    pet: PropTypes.object.isRequired,
-    setPetData: PropTypes.func.isRequired,
-  };
-
-  console.log('Existing pet data, rendering CurrentPet');
-
-  function deletePet() {
-    if (confirm('Are you sure you want to delete your pet? This action is irreversible.')) {
-      fetch('/delete-pet')
-        .then((response) => response.json())
-        .then((msg) => {
-          console.log('deleting pet');
-          alert(msg);
-          setPetData(null);
-        })
-        .catch((error) => alert(error.toString()));
-    } else {
-      alert('Your pet has not been deleted.');
-    }
-  }
-
-  return (
-    <div>
-      <h1>Your Pet</h1>
-      <h2>{pet.name} the {pet.personality} {pet.species_name}</h2>
-      <h3 id="location">Location: {pet.city}, {pet.region}, {pet.country}</h3>
-      <PetDisplay pet={pet} />
-
-      <button type="button" id="delete-pet" onClick={deletePet}>DELETE PET</button>
-    </div>
-  );
-}
-
 function VirtualPetApp() {
   const [petData, setPetData] = React.useState(undefined);
 
