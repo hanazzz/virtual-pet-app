d7dc3b897292444afbd1c0482d7ac55e8d0ded82
Hana Zait <hana.o.zait@gmail.com>
2022-09-23 12:41:20 -0700

    Updated return logic to be more concise and less repetitive

--- static/react/app/App.jsx
+++ static/react/app/App.jsx
@@ -4,6 +4,7 @@
 
 function VirtualPetApp() {
   const [petData, setPetData] = React.useState(undefined);
+  const username = localStorage.getItem('username');
 
   console.log('Loading app');
 
@@ -24,31 +25,67 @@ function VirtualPetApp() {
       .catch((error) => alert(error.toString()));
   }, []);
 
-  // If user has pet
+  // If user's pet status is unknown (i.e. useEffect hasn't run)
+  let appContent = (<div>Loading...</div>);
+
+  // If user has pet, load CurrentPet. If not, load PetGene
   if (petData) {
-    alert('Your pet is so cute!');
-    return (
+    appContent = (
       <CurrentPet
         pet={petData}
         setPetData={setPetData}
       />
     );
-  // If user doesn't have pet
   } else if (petData === null) {
-    console.log('NO pet data, rendering PetGenerator');
-    alert("Looks like you don't have a pet yet! Let's fix that.");
-    return (
+    appContent = (
       <PetGenerator
         petData={petData}
         setPetData={setPetData}
       />
     );
   }
-  // If user's pet status is unknown (i.e. useEffect hasn't run)
-  console.log('the void');
+
   return (
-    <div>Loading...</div>
+    <>
+      <Navbar username={username} />
+      {appContent}
+    </>
   );
+
+  // // If user has pet
+  // if (petData) {
+  //   alert('Your pet is so cute!');
+  //   return (
+  //     <>
+  //       <Navbar username={petData.username} />
+  //       <CurrentPet
+  //         pet={petData}
+  //         setPetData={setPetData}
+  //       />
+  //     </>
+  //   );
+  // // If user doesn't have pet
+  // } else if (petData === null) {
+  //   console.log('NO pet data, rendering PetGenerator');
+  //   alert("Looks like you don't have a pet yet! Let's fix that.");
+  //   return (
+  //     <>
+  //       <Navbar username={username} />
+  //       <PetGenerator
+  //         petData={petData}
+  //         setPetData={setPetData}
+  //       />
+  //     </>
+  //   );
+  // }
+  // // If user's pet status is unknown (i.e. useEffect hasn't run)
+  // console.log('the void');
+  // return (
+  //   <>
+  //     <Navbar username={username} />
+  //     <div>Loading...</div>
+  //   </>
+  // );
 }
 
 ReactDOM.render(<VirtualPetApp />, document.querySelector('#app'));
