daafa9dde422089ead8977b66ebd13e034adef87
Hana Zait <hana.o.zait@gmail.com>
2022-10-12 19:25:51 -0700

    Re-styled species toggle to more clearly indicate what is selected

--- static/react/app/generatePet/SpeciesToggle.jsx
+++ static/react/app/generatePet/SpeciesToggle.jsx
@@ -7,10 +7,10 @@ function SpeciesToggle({ useCustomSpecies, setUseCustomSpecies }) {
   }
 
   return (
-    <div className="btn-group my-6">
-      <input type="radio" name="species-choice" data-title="Use existing pet species" aria-label="Use existing pet species" value="existing" onInput={handleSpeciesChange} className="btn shrink md:shrink-0" defaultChecked />
+    <div className="btn-group mt-8 mb-2 md:my-6">
+      <input type="radio" name="species-choice" data-title="Use existing pet species" aria-label="Use existing pet species" value="existing" onInput={handleSpeciesChange} className="btn shrink lg:shrink-0" defaultChecked />
 
-      <input type="radio" name="species-choice" data-title="Create custom pet species" aria-label="Create custom pet species" value="custom" onInput={(evt) => handleSpeciesChange(evt)} className="btn shrink md:shrink-0" />
+      <input type="radio" name="species-choice" data-title="Create custom pet species" aria-label="Create custom pet species" value="custom" onInput={(evt) => handleSpeciesChange(evt)} className="btn shrink lg:shrink-0" />
     </div>
   );
 }
