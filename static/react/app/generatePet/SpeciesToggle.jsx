c97ea134398b95cbbef4df15505b224f9d197e51
Hana Zait <hana.o.zait@gmail.com>
2022-10-18 22:19:35 -0700

    Added width specification to species toggle div

--- static/react/app/generatePet/SpeciesToggle.jsx
+++ static/react/app/generatePet/SpeciesToggle.jsx
@@ -7,7 +7,7 @@ function SpeciesToggle({ useCustomSpecies, setUseCustomSpecies }) {
   }
 
   return (
-    <div className="btn-group mt-8 mb-2 md:my-6">
+    <div className="btn-group w-fit mt-8 mb-2 md:my-6">
       <input type="radio" name="species-choice" data-title="Use existing pet species" aria-label="Use existing pet species" value="existing" onInput={handleSpeciesChange} className="btn shrink lg:shrink-0" defaultChecked />
 
       <input type="radio" name="species-choice" data-title="Create custom pet species" aria-label="Create custom pet species" value="custom" onInput={(evt) => handleSpeciesChange(evt)} className="btn shrink lg:shrink-0" />
