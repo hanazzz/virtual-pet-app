8c8cecdd2d83b6b9fb50cf0ad4ba3065f93a6b86
Hana Zait <hana.o.zait@gmail.com>
2022-10-20 23:04:23 -0700

    Changed positioning of species toggle btns for mobile and center aligned weather display text

--- static/react/app/currentPet/WeatherDisplay.jsx
+++ static/react/app/currentPet/WeatherDisplay.jsx
@@ -37,7 +37,7 @@ function WeatherDisplay({ lat, lon, tempInF }) {
     <Card id="weather" color="secondary" addlClasses="card-compact w-fit mt-6 md:mt-0 mx-auto md:mx-0" addlBodyClasses="gap-1 justify-center">
       <div id="weather-description" className="text-base">{currentWeather.description}</div>
 
-      <div className="flex flex-row justify-start font-bold">
+      <div className="flex flex-row justify-start items-center font-bold">
         {/* weather icon */}
         <i className={`wi wi-owm-${currentWeather.conditionCode} pr-4 text-4xl`} />
         {/* if tempInF is true, display temperature in Fahrenheit, else display in Celsius */}
